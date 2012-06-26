40630b3c8f661b8f9bbe8c5621531c0d35877555
mfiguiere <mfiguiere@Michaels-MacBook-Pro.local>
2012-06-26 19:18:01 +0200

    fix reducer

--- src/main/java/com/soat/hadoopworkshop/ex2/AnagramReducer.java
+++ src/main/java/com/soat/hadoopworkshop/ex2/AnagramReducer.java
@@ -9,15 +9,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class AnagramReducer extends
 		Reducer<Text, IntWritable, Text, IntWritable> {
 
-	@Override
-	public void reduce(Text key, Iterable<IntWritable> values, Context context)
-			throws IOException, InterruptedException {
-
-		int count = 0;
-		for (IntWritable value : values) {
-			count += value.get();			
-		}
-		context.write(key, new IntWritable(count));
 	}
 
 }
